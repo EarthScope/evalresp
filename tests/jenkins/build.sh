12b29d118fd8163a896d5e9912fb7539b4210a8b
andrew cooke <andrewcooke@isti.com>
2017-04-05 11:47:45 -0400

    more details for build

--- tests/jenkins/build.sh
+++ tests/jenkins/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+# previously (in jenkins directly)
+# git clone git@github.com-evalresp:iris-edu/evalresp.git
+# where github.com-evalresp is defined in ~/.ssh/config according to
+# https://gist.github.com/jexchan/2351996
+
 if [ ! -d src ]; then
     echo "run this script in the top level directory"
     exit 1
