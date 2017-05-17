038cd41d84c692266b2442798769261f294e8e10
Dylan Thies <dylanthies@isti.com>
2017-05-17 16:16:31 -0400

    Initial commits of logging functions
    working on #3
    put intial version of the functions in code as well as necessary
    structures and datatypes in header

---
+++ src/log.h
@@ -0,0 +1,30 @@
+#ifndef __EVALRESP_LOG_H__
+#define __EVALRESP_LOG_H__
+
+
+#ifndef MAX_LOG_MSG_LEN 
+#define MAX_LOG_MSG_LEN 256
+#endif /* MAX_LOG_MSG_LEN  */
+
+typedef struct evalresp_log_msg
+{
+    char msg[MAX_LOG_MSG_LEN];
+    int log_level;
+    int verbosity_level;
+    time_t timestamp;
+} evalresp_log_msg_t;
+
+typedef int (*evalresp_log_func_t)(evalresp_log_msg_t *);
+
+typedef enum log_level_ref
+{
+    ERROR=0,
+    WARN,
+    INFO,
+    DEBUG
+} log_level_ref_t;
+const char *log_level_strs[]={"ERROR", "WARN", "INFO", "DEBUG"};
+
+extern int evalresp_log (evalresp_log_func_t, int, int, char *, ...);
+
+#endif /* __EVALRESP_LOG_H__ */
---
+++ src/log/log.c
@@ -0,0 +1,28 @@
+#include<evalresp/log.h>
+#include<time.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+
+int evalresp_log (evalresp_log_func_t log_func, int level, int verbosity, char *fmt, ...)
+{
+    evalresp_log_msg_t msg[1];
+    char date_str[256];/*TODO this is tomany bytes*/
+    va_list p_args;
+    va_start(p_args, fmt);
+    vsnprintf(msg->msg, MAX_LOG_MSG_LEN, fmt, args);
+    va_end(p_args)
+    msg->log_level=level;
+    msg->verbosity_level=verbosity;
+    msg->timestamp=time(NULL);
+    if (!log_func)
+    {
+        return log_func(msg);
+    }
+    strftime(date_str, 256, "%c", localtime(&(msg->timestamp)));
+    fprintf(stderr, "%s [%s] %s\n", date_str,
+            (level < 4 && level >= 0) ? log_level_strs[level] : "unknown",
+            msg->msg);
+    return EXIT_SUCCESS;
+}
+
