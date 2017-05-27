11ca984968fb2ba5944f76e1b66521537a25e1b7
HE <he-hesce@users.noreply.github.com>
2017-05-27 15:25:34 +0000

    Doxygenize cubic spline interpolation function.

--- src/evr_spline.h
+++ src/evr_spline.h
@@ -1,42 +1,56 @@
-/*
-   evr_spline():  Cubic spline interpolation.  Two "source" arrays (abscissa
-                  'X' and ordinate 'Y' values) and an array of "new"
-                  abscissa values are given; an array of "destination"
-                  ordinate values is generated and returned.  Any "new"
-                  abscissa values outside of the range of "source" abscissa
-                  values are ignored, resulting in a reduced-size
-                  "destination" ordinate array being generated and an
-                  error message being generated.
-     num_points:  number of points in given "source" arrays.
-     t:  abscissa "source" array of 'double' values.
-     y:  ordinate "source" array of 'double' values.
-     tension:  tension value for interpolation, use 0.0 as default vlaue:
-               If tension=0 then a spline with tension reduces to a
-               conventional piecewise cubic spline.  In the limits
-               tension->+infinity and tension->-infinity, a spline with
-               tension reduces to a piecewise linear (`broken line')
-               interpolation.  To oversimplify a bit, 1.0/tension is the
-               maximum abscissa range over which the spline likes to curve,
-               at least when tension>0.  So increasing the tension far above
-               zero tends to make the spline contain short curved sections,
-               separated by sections that are almost straight.  The curved
-               sections will be centered on the user-specified data points.
-               The behavior of the spline when tension<0 is altogether
-               different: it will tend to oscillate, though as
-               tension->-infinity the oscillations are damped out.
-     k:  boundary condition, use 1.0 as default value:  Appears in the two
-               boundary conditions y''[0]=ky''[1] and y''[n]=ky''[n-1].
-     xvals_arr:  array of "new" abscissa values to use with interpolation
-                 ('double' values).
-     num_xvals:  number of entries in 'xvals_arr'.
-     **p_retvals_arr:  reference to ordinate "destination" array of 'double'
-                       values generated via interpolation.
-     *p_num_retvals:  reference to number of values returned in
-                      'p_retvals_arr' (will be less than 'num_xvals'
-                      if any new abscissa values are out of range).
-   Returns:  NULL if successful; an error message string if not.
-*/
-char *evr_spline(int num_points, double *t, double *y, double tension, double k,
-        double *xvals_arr, int num_xvals, double **p_retvals_arr,
-        int *p_num_retvals);
+/**
+ * @mainpage Introduction
+ *
+ * @section purpose Purpose
+ *
+ * Implements a cubic spline interpolation interface for evalresp.
+ *
+ * @section history History
+ *
+ * Written by <a href="http://www.isti.com/">Instrumental Software
+ * Technologies, Inc.</a> (ISTI) in 2017.
+ */
 
+/**
+ * @defgroup evalresp_spline evalresp Private Spline Interpolation Interface
+ * @ingroup evalresp
+ * @brief Private cubic spline interpolation interface for evalresp.
+ */
+
+/**
+ * @file
+ * @brief This file contains declarations and global structures for evalresp
+ *        cubic spline interpolation.
+ */
+
+/**
+ * @private
+ * @ingroup evalresp_spline
+ * @brief Cubic spline interpolation.
+ * @details Two "source" arrays (abscissa 'X' and ordinate 'Y' values) and an
+ *          array of "new" abscissa values are given; an array of
+ *          "destination" ordinate values is generated and returned. Any
+ *          "new" abscissa values outside of the range of "source" abscissa
+ *          values are ignored, resulting in a reduced-size "destination"
+ *          ordinate array being generated and an error message being
+ *          generated.
+ * @param[in] num_points Number of points in given "source" arrays.
+ * @param[in] t Abscissa "source" array of 'double' values.
+ * @param[in] y Ordinate "source" array of 'double' values.
+ * @param[in] tension Not used anymore.
+ * @param[in] k Not used anymore.
+ * @param[in] xvals_arr Array of "new" abscissa values to use with
+ *                      interpolation ('double' values).
+ * @param[in] num_xvals Number of entries in 'xvals_arr'.
+ * @param[in] p_retvals_arr Reference to ordinate "destination" array of
+ *                          'double' values generated via interpolation.
+ * @param[in] p_num_retvals reference to number of values returned in
+ *                          'p_retvals_arr' (will be less than 'num_xvals' if
+ *                          any new abscissa values are out of range).
+ * @return @c NULL if successful; an error message string if not.
+ */
+char * evr_spline(int num_points, double *t, double *y,
+                  double tension, /* Not used anymore */
+                  double k, /* Not used anymore */
+                  double *xvals_arr, int num_xvals,
+                  double **p_retvals_arr, int *p_num_retvals);
