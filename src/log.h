52c0756817dcbd442ad9b400f609940e08e53ea6
HE <he-hesce@users.noreply.github.com>
2017-05-27 13:35:55 +0000

    Refresh log Doxygen comments for new Doxygen setup.

--- src/log.h
+++ src/log.h
@@ -1,3 +1,30 @@
+/**
+ * @mainpage Introduction
+ *
+ * @section purpose Purpose
+ *
+ * Implements a private logging interface for evalresp.
+ *
+ * @section history History
+ *
+ * Written by <a href="http://www.isti.com/">Instrumental Software
+ * Technologies, Inc.</a> (ISTI) in 2017.
+ */
+
+/**
+ * @defgroup evalresp_log evalresp Private Logging Interface
+ * @ingroup evalresp
+ * @brief Private logging interface for evalresp.
+ * @author 2017: Dylan Thies, ISTI.
+ */
+
+/**
+ * @file
+ * @brief This file contains declarations and global structures for evalresp
+ *        logging.
+ * @author 2017: Dylan Thies, ISTI.
+ */
+
 #ifndef __EVALRESP_LOG_H__
 #define __EVALRESP_LOG_H__
 
@@ -7,13 +34,12 @@
 #ifndef MAX_LOG_MSG_LEN
 #define MAX_LOG_MSG_LEN 256
 #endif /* MAX_LOG_MSG_LEN  */
-/** @file log.h
- *  @author Dylan Thies
- *  @ brief header for evalresp logging function
- */
 
 /**
- * @brief Object created by evalresp library logging functions that will be passed to  logging function if provided.
+ * @private
+ * @ingroup evalresp_log
+ * @brief Object created by evalresp library logging functions that will be
+ *        passed to logging function if provided.
  */
 typedef struct evalresp_log_msg
 {
@@ -23,18 +49,32 @@ typedef struct evalresp_log_msg
     time_t timestamp; /**< seconds since epoch that this message was created */
 } evalresp_log_msg_t;
 
-/** @brief A convience data type of the logging function. */
+/**
+ * @private
+ * @ingroup evalresp_log
+ * @brief A convience data type of the logging function.
+ */
 typedef int (*evalresp_log_func_t)(evalresp_log_msg_t *, void *);
 
-/** @brief data type that is for creating a persistent log object 
- * This while be set up by the user of the library. with the information it needs to overwrite logging */
+/**
+ * @private
+ * @ingroup evalresp_log
+ * @brief Data type that is for creating a persistent log object.
+ *
+ * This while be set up by the user of the library. with the information it
+ * needs to overwrite logging
+ */
 typedef struct evalresp_log
 {
     evalresp_log_func_t log_func; /**< the function that the loger should call back */
     void *func_data; /**< a pointer to the data portion that should be sent to the callback function */
 } evalresp_log_t;
 
-/** @brief an enum of the different logging levels to expect in evalresp */
+/**
+ * @private
+ * @ingroup evalresp_log
+ * @brief an enum of the different logging levels to expect in evalresp
+ */
 typedef enum log_level_ref
 {
     ERROR=0, /**< level when reporting an error */
@@ -43,21 +83,29 @@ typedef enum log_level_ref
     DEBUG /**< level for the most information */
 } log_level_ref_t;
 
-/** Array  of the log levels  in plain text 
- * Using the values from log_level_ref_t will directly corespond to the plaintext.
- * Eg, log_level_strs[ERROR] == "DEBUG" */
+/**
+ * @private
+ * @ingroup evalresp_log
+ * @brief Array of the log levels in plain text.
+ * 
+ * Using the values from log_level_ref_t will directly corespond to the
+ * plaintext. Eg, log_level_strs[ERROR] == "DEBUG".
+ */
 extern const char *log_level_strs[];
 
-
 /* log.c */
+
 /**
- * @brief  A function to send log message to log using the log structure.
+ * @private
+ * @ingroup evalresp_log
+ * @brief A function to send log message to log using the log structure.
  *
  * This function is used in the evalresp library to allow external developers
- * to pass their own logging function as well as data needed for the function to operate.
- *
- * example use of how to use, note the data is null because the
- * log_func reqires data to be null in this case
+ * to pass their own logging function as well as data needed for the function
+ * to operate.
+ * 
+ * Example use of how to use, note the data is null because the log_func
+ * reqires data to be null in this case.
 
  @verbatim
  #include "log.h"
@@ -92,10 +140,12 @@ extern const char *log_level_strs[];
  }
 @endverbatim
 
- * In this Example log_func is the user defined function that will be passed to evalresp_log. 
+ * In this Example log_func is the user defined function that will be passed
+ * to evalresp_log.
  *
- * To develop against libevalresp
- @verbatim
+ * To develop against libevalresp:
+
+@verbatim
 #include <log.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -126,11 +176,13 @@ int main(int argc, char *argv[])
 }
 @endverbatim
 
- * evalresp_function is a made up function but will be typical of functions that use evalresp_log
+ * evalresp_function is a made up function but will be typical of functions
+ * that use evalresp_log
  *
- * @param[in] log structure containing the information for the logging function and data required to control the log
+ * @param[in] log structure containing the information for the logging
+ *                function and data required to control the log
  * @param[in] level the logging level the msg is intended for
- * $param[in] verbosity the verbosity level the mesg is intended for
+ * @param[in] verbosity the verbosity level the mesg is intended for
  * @param[in] fmt a format string that follows the printf style format
  * @param[in] ... any additional variables needed for fmt
  * @retval EXIT_SUCCESS when succesfully called
@@ -139,14 +191,18 @@ int main(int argc, char *argv[])
  * @sa { evalresp_log_t evalresp_log_t_alloc evalresp_log_t_free evalresp_log_t_init evalresp_log_basic evalresp_log_v }
  */
 extern int evalresp_log (evalresp_log_t*, int, int, char *, ...);
+
 /**
+ * @private
+ * @ingroup evalresp_log
  * @brief send log message to log using explicit log function pointer and data
  *
  * This function is used in the evalresp library to allow external developers
- * to pass their own logging function as well as data needed for the function to operate.
+ * to pass their own logging function as well as data needed for the function
+ * to operate.
  *
- * example use of how to use the log, note the data is null because the
- * log_func reqires data to be null in this case
+ * Example use of how to use the log, note the data is null because the
+ * log_func requires data to be null in this case:
 
 @verbatim
  #include "log.h"
@@ -192,11 +248,15 @@ extern int evalresp_log (evalresp_log_t*, int, int, char *, ...);
  * \sa { evalresp_log_t evalresp_log_t_alloc evalresp_log_t_free evalresp_log_t_init evalresp_log_basic evalresp_log_v }
  */
 extern int evalresp_log_basic (evalresp_log_func_t, void *, int, int, char *, ...);
+
 /**
- * @brief send log message to log using base parameters and va_list instead of printf like varadic function
+ * @private
+ * @ingroup evalresp_log
+ * @brief Send log message to log using base parameters and va_list instead of
+ *        printf like varadic function
  *
- * this is an extension of evalresp_log_basic that takes a va_list, really should only be used by a wrap[ping function
- * for the logging, suchas evalresp_log and evalresp_log_basic
+ * This is an extension of evalresp_log_basic that takes a va_list, really
+ * should only be used by a wrap[ping function for the logging, such as evalresp_log and evalresp_log_basic
  *
  * @param[in] log_func the logging function used for controlled output of log
  * @param[in] log_func_data data needed by the logging function to better control the logging function
@@ -213,9 +273,12 @@ extern int evalresp_log_v (evalresp_log_func_t, void *, int, int, char *, va_lis
 
 /* log/helpers.c */
 /**
- * @brief Allocate a evalresp_log_t object and initialize it to values passed to this function
+ * @private
+ * @ingroup evalresp_log
+ * @brief Allocate a evalresp_log_t object and initialize it to values passed
+ *        to this function.
  *
- * The resulting object will be created using malloc so be sure to free it
+ * The resulting object will be created using malloc so be sure to free it.
  *
  * @param[in] log_func evalresp_log_func_t or function pointer to logging function to use.
  * @param[in] func_data additional data needed to control log_func
@@ -223,7 +286,10 @@ extern int evalresp_log_v (evalresp_log_func_t, void *, int, int, char *, va_lis
  * @retval NULL on error
  */
 extern evalresp_log_t *evalresp_log_t_alloc(evalresp_log_func_t, void *);
+
 /**
+ * @private
+ * @ingroup evalresp_log
  * @brief Free a log object.
  *
  * Currently a wrapper for free().
@@ -231,7 +297,10 @@ extern evalresp_log_t *evalresp_log_t_alloc(evalresp_log_func_t, void *);
  * @param[in] log
  */
 extern void evalresp_log_t_free(evalresp_log_t *);
+
 /**
+ * @private
+ * @ingroup evalresp_log
  * @brief Initalize an already allocated evalresp_log_t object with the log_func and func_data.
  *
  * @param[in,out] log pointer to log object being initialized
