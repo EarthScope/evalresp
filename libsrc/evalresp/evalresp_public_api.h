3b35b45c418462167c30a67caea1ad253bd30c8c
andrew cooke <andrewcooke@isti.com>
2017-06-14 13:01:44 -0400

    outline public api

---
+++ libsrc/evalresp/evalresp_public_api.h
@@ -0,0 +1,71 @@
+
+#ifndef EVALRESP_PUBLIC_API_H
+#define EVALRESP_PUBLIC_API_H
+
+#include "evalresp/evalresp_public_seed.h"
+#include "evalresp_log/log.h"
+
+// TODO - see design doc for details that should go into comments
+
+typedef struct {
+} evalresp_filter;
+
+typedef struct {
+} evalresp_channels;
+
+// TODO - sncl may want expanding
+int new_filter(evalresp_log_t *log, const char *sncl, int year, int julian_day,
+		evalresp_filter **filter);
+
+int free_filter(evalresp_filter **filter);
+
+int add_constraint(evalresp_log_t *log, const char *snclq, evalresp_filter **filter);
+
+int evalresp_char_to_channels(evalresp_log_t *log, const char *seed_or_xml,
+		const evalresp_filter* filter, evalresp_channels *channels);
+
+int evalresp_file_to_channels(evalresp_log_t *log, const char *filename,
+		const evalresp_filter* filter, evalresp_channels *channels);
+
+typedef struct {
+
+} evalresp_result;
+
+typedef struct {
+
+} evalresp_results;
+
+typedef struct {
+
+} evalresp_eval_options;
+
+int evalresp_channel_to_result(evalresp_log_t *log, const evalresp_channel *channel,
+		const evalresp_eval_options *eval_options, evalresp_result **result);
+
+int evalresp_channels_to_results(evalresp_log_t *log, const evalresp_channels *channels,
+		const evalresp_eval_options *eval_options, evalresp_results **results);
+
+typedef enum {
+	evalresp_frequency_format,
+	evalresp_amplitude_format,
+	evalresp_phase_format,
+	evalresp_complex_format}
+evalresp_format;
+
+int evalresp_result_to_char(evalresp_log_t *log, const evalresp_result *result,
+		evalresp_format format, char **output);
+
+int evalresp_result_to_file(evalresp_log_t *log, const evalresp_result *result,
+		evalresp_format format, const char *filename);
+
+typedef struct {
+
+} evalresp_output_options;
+
+int evalresp_results_to_dir(evalresp_log_t *log, const evalresp_results *results,
+		evalresp_output_options *output_options, const char *dir);
+
+int evalresp_dir_to_dir(evalresp_log_t *log, const char *dir,
+		evalresp_eval_options *eval_options, evalresp_output_options *output_options);
+
+#endif
---
+++ libsrc/evalresp/evalresp_public_compat.h
@@ -0,0 +1,137 @@
+
+#ifndef EVALRESP_PUBLIC_COMPAT_H
+#define EVALRESP_PUBLIC_COMPAT_H
+
+#include "evalresp/evalresp_public_seed.h"
+
+// TODO - this should become evresp.h eventually
+
+// once this is defined (v4.0.6 onwards), some functions take an additional
+// xml flag argument
+#define EVRESP_XML
+
+/**
+ * @private
+ * @ingroup evalresp_private
+ * @brief Evaluate responses for user requested station/channel/network tuple
+ *        at the frequencies requested by the user.
+ * @param[in] stalst Station list.
+ * @param[in] chalst Channel list.
+ * @param[in] net_code Network code.
+ * @param[in] locidlst Localtion ID list.
+ * @param[in] date_time Date + time.
+ * @param[in] units Units.
+ * @param[in] file File name.
+ * @param[in] freqs Frequency vector.
+ * @param[in] nfreqs Number of frequencies in the vector.
+ * @param[in] rtype Use complex value or Amplited/phase in the output.
+ * @param[in] verbose If used, then verbose.
+ * @param[in] start_stage Start stage.
+ * @param[in] stop_stage End stage.
+ * @param[in] stdio_flag Print output to stdio if used.
+ * @param[in] useTotalSensitivityFlag Use or not total sensitivity.
+ * @param[in] x_for_b62 Frequency value for Polynomial.
+ * @param[in] xml_flag Use XML or not.
+ * @param[in] log Logging structure.
+ * @remark Calls evresp_itp() but with listinterp_tension set to 0.
+ * @returns Responses.
+ */
+struct response *evresp (char *stalst, char *chalst, char *net_code,
+                         char *locidlst, char *date_time, char *units,
+                         char *file, double *freqs, int nfreqs, char *rtype,
+                         char *verbose, int start_stage, int stop_stage,
+                         int stdio_flag, int useTotalSensitivityFlag,
+                         double x_for_b62, int xml_flag, evalresp_log_t *log);
+
+/**
+ * @private
+ * @ingroup evalresp_private
+ * @brief Evaluate responses for user requested station/channel/network tuple
+ *        at the frequencies requested by the user.
+ * @param[in] stalst Station list.
+ * @param[in] chalst Channel list.
+ * @param[in] net_code Network code.
+ * @param[in] locidlst Localtion ID list.
+ * @param[in] date_time Date + time.
+ * @param[in] units Units.
+ * @param[in] file File name.
+ * @param[in] freqs Frequency vector.
+ * @param[in] nfreqs Number of frequencies in the vector.
+ * @param[in] rtype Use complex value or Amplited/phase in the output.
+ * @param[in] verbose If used, then verbose.
+ * @param[in] start_stage Start stage.
+ * @param[in] stop_stage End stage.
+ * @param[in] stdio_flag Print output to stdio if used.
+ * @param[in] listinterp_out_flag Interpolate output of list (B55).
+ * @param[in] listinterp_in_flag Interpolate input of list (B55).
+ * @param[in] listinterp_tension This parameter is obsolote and should be removed.
+ * @param[in] useTotalSensitivityFlag Use or not total sensitivity.
+ * @param[in] x_for_b62  Frequency value for Polynomial.
+ * @param[in] xml_flag Use XML or not.
+ * @param[in] log Logging structure.
+ * @returns Responses.
+ */
+struct response *evresp_itp (char *stalst, char *chalst, char *net_code,
+                             char *locidlst, char *date_time, char *units,
+                             char *file, double *freqs, int nfreqs,
+                             char *rtype, char *verbose, int start_stage,
+                             int stop_stage, int stdio_flag,
+                             int listinterp_out_flag, int listinterp_in_flag,
+                             double listinterp_tension,
+                             int useTotalSensitivityFlag, double x_for_b62,
+                             int xml_flag, evalresp_log_t *log);
+
+/**
+ * @private
+ * @ingroup evalresp_private
+ * @brief Evaluate responses for user requested station/channel/network tuple
+ *        at the frequencies requested by the user. FORTRAN callable interface to the evresp.
+ *
+ *        This routine was updated in release 4.0.0 to support Fortran
+ *        95.  Previous versions were clearly broken (parameters had
+ *        been added with no respect to the implicit lengths from
+ *        Fortran character arrays), so backwards compatibility is not
+ *        provided.
+ *        Given the cleaner interface supported by Fortran 95, this
+ *        routine can also be called from C.
+ *        Whereas the other function returns a linked list of responses
+ *        one for each response that matched the user's request), this
+ *        routine returns the response for one (1)
+ *        station-channel-network for one (1) effective time.  If more
+ *        than one match is found for a given
+ *        station-channel-network-time, an error condition is raised
+ *        (and a value of -1 is returned to the calling routine to
+ *        indicate failure).  Likewise, a value of 1 is returned if no
+ *        match is found for the given station-channel-network-time.  If
+ *        a unique match is found, a value of 0 is returned to the
+ *        calling routine
+ *
+ * @param[in] sta Station.
+ * @param[in] cha Channel.
+ * @param[in] net Network.
+ * @param[in] locid Localtion ID.
+ * @param[in] datime data and time.
+ * @param[in] units Units.
+ * @param[in] file File name.
+ * @param[in] freqs Frequency vector.
+ * @param[in] nfreqs Number of frequencies in the vector.
+ * @param[out] resp Double vector containing complex response (even elements are real).
+ * @param[in] rtype Use complex value or Amplited/phase in the output.
+ * @param[in] verbose If used, then verbose.
+ * @param[in] start_stage Start stage..
+ * @param[in] stop_stage End stage..
+ * @param[in] stdio_flag Print output to stdio if used.
+ * @param[in] useTotalSensitivityFlag Use or not total sensitivity.
+ * @param[in] x_for_b62 Frequency value for Polynomial.
+ * @param[in] xml_flag Use XML or not.
+ * @remark Fortran interface.
+ * @note this will log to default of the library, currently stderr
+ */
+int evresp_1 (char *sta, char *cha, char *net, char *locid, char *datime,
+              char *units, char *file, double *freqs, int nfreqs, double *resp,
+              char *rtype, char *verbose, int start_stage, int stop_stage,
+              int stdio_flag, int useTotalSensitivityFlag, double x_for_b62,
+              int xml_flag);
+
+
+#endif
