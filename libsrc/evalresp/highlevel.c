EarthScope/evalresp

commit 974d67644e2893fb0ba458c3ca7cb9a1226e59f7

Fixed stdio problem
was not handling the case with only one additional flag
It was returning an ap output format when it should've been fap
then robot was not calling the right check on it, it was trying
to compare 2 rows of floats but the output meant to have 3

libsrc/evalresp/highlevel.c
@@ -9,7 +9,7 @@
 
 // new code giving a high level interface.
 
-static char *prefixes[] = {"FAP", "AMP", "PHASE", "SPECTRA"};
+static char *prefixes[] = {"FAP", "AMP", "PHASE", "SPECTRA", "AMP/PHS"};
 
 #define FILENAME_TEMPLATE "%s.%s.%s.%s.%s"
 
@@ -18,7 +18,7 @@ print_file (evalresp_log_t *log, evalresp_file_format format,
             int use_stdio, const evalresp_response *response)
 {
   int status = EVALRESP_OK, length;
-  char *filename = NULL, *prefix = prefixes[format];
+  char *filename = NULL, *prefix = prefixes[use_stdio && evalresp_fap_output_format ? 4 : format];
   length = snprintf (filename, 0, FILENAME_TEMPLATE, prefix,
                      response->network, response->station, response->locid, response->channel);
   if (!(filename = calloc (length + 1, sizeof (*filename))))
@@ -32,10 +32,11 @@ print_file (evalresp_log_t *log, evalresp_file_format format,
                     response->network, response->station, response->locid, response->channel);
     if (use_stdio)
     {
-      fprintf (stdout, "--------------------------------------------------\n");
-      fprintf (stdout, "%s\n", filename);
-      fprintf (stdout, "--------------------------------------------------\n");
+      fprintf (stdout, " --------------------------------------------------\n");
+      fprintf (stdout, " %s\n", filename);
+      fprintf (stdout, " --------------------------------------------------\n");
       status = evalresp_response_to_stream (log, response, format, stdout);
+      fprintf (stdout, " --------------------------------------------------\n");
     }
     else
     {

libsrc/evalresp/output.c
@@ -77,13 +77,13 @@ evalresp_response_to_char (evalresp_log_t *log, const evalresp_response *respons
       switch (format)
       {
       case evalresp_fap_file_format:
-        length += snprintf (NULL, 0, "%.6E  %.6E  %.6E\n", freq_arr[i], amp_arr[i], pha_arr[i]);
+        length += snprintf (NULL, 0, "%.6E %.6E %.6E\n", freq_arr[i], amp_arr[i], pha_arr[i]);
         break;
       case evalresp_amplitude_file_format:
-        length += snprintf (NULL, 0, "%.6E  %.6E\n", freq_arr[i], amp_arr[i]);
+        length += snprintf (NULL, 0, "%.6E %.6E\n", freq_arr[i], amp_arr[i]);
         break;
       case evalresp_phase_file_format:
-        length += snprintf (NULL, 0, "%.6E  %.6E\n", freq_arr[i], pha_arr[i]);
+        length += snprintf (NULL, 0, "%.6E %.6E\n", freq_arr[i], pha_arr[i]);
         break;
       case evalresp_complex_file_format:
         length += snprintf (NULL, 0, "%.6E  %.6E  %.6E\n", freq_arr[i], response->rvec[i].real, response->rvec[i].imag);
@@ -109,13 +109,13 @@ evalresp_response_to_char (evalresp_log_t *log, const evalresp_response *respons
       switch (format)
       {
       case evalresp_fap_file_format:
-        offset += snprintf (*output + offset, length - offset, "%.6E  %.6E  %.6E\n", freq_arr[i], amp_arr[i], pha_arr[i]);
+        offset += snprintf (*output + offset, length - offset, "%.6E %.6E %.6E\n", freq_arr[i], amp_arr[i], pha_arr[i]);
         break;
       case evalresp_amplitude_file_format:
-        offset += snprintf (*output + offset, length - offset, "%.6E  %.6E\n", freq_arr[i], amp_arr[i]);
+        offset += snprintf (*output + offset, length - offset, "%.6E %.6E\n", freq_arr[i], amp_arr[i]);
         break;
       case evalresp_phase_file_format:
-        offset += snprintf (*output + offset, length - offset, "%.6E  %.6E\n", freq_arr[i], pha_arr[i]);
+        offset += snprintf (*output + offset, length - offset, "%.6E %.6E\n", freq_arr[i], pha_arr[i]);
         break;
       case evalresp_complex_file_format:
         offset += snprintf (*output + offset, length - offset, "%.6E  %.6E  %.6E\n", freq_arr[i], response->rvec[i].real, response->rvec[i].imag);

libsrc/evalresp/public_api.h
@@ -143,6 +143,7 @@ typedef struct
   int station_xml;
   evalresp_output_format format;
   evalresp_unit unit;
+  char format_set;
 } evalresp_options;
 
 int evalresp_new_options (evalresp_log_t *log, evalresp_options **options);

src/evalresp.c
@@ -144,7 +144,7 @@ parse_args (int argc, char *argv[], evalresp_options *options, evalresp_filter *
     }
   }
 
-  if (argc - 1 > first_switch)
+  if (argc - 1 >= first_switch)
   {
     flags_argc = argc - first_switch + 1;
     flags_argv = argv + first_switch - 1;
@@ -183,6 +183,7 @@ parse_args (int argc, char *argv[], evalresp_options *options, evalresp_filter *
 
       case 'r':
         status = evalresp_set_format (*log, options, optarg);
+        options->format_set = 1;
         break;
 
       case 'S':
@@ -230,6 +231,10 @@ parse_args (int argc, char *argv[], evalresp_options *options, evalresp_filter *
         status = EVALRESP_ERR;
       }
     }
+  }
+  if (!options->format_set && options->use_stdio)
+  {
+    options->format = evalresp_fap_output_format;
   }
 
   if (!status)

tests/robot/all/base/01_args.robot
@@ -100,7 +100,7 @@ Stage 3 - 5
 Stdio
     Prepare  base/args/stdio  base  RESP.Z.CGV..HYZ
     Run process  evalresp CGV HYZ 2010 1 0.001 10 100 -stdio < RESP.Z.CGV..HYZ  shell=True  stdout=stdout  stderr=stderr
-    Count and compare target files two float cols
+    Count and compare target files n float cols  3
 
 Estimated delay
     Prepare  base/args/delay  base  RESP.Z.CGV..HYZ

commit 3fe989486da3fb5d7c84a3e900afcc36f97826d7

fixing some issues introduced in 974d67644e2893fb0ba458c3ca7cb9a1226e59f7

libsrc/evalresp/highlevel.c
@@ -19,7 +19,7 @@ print_file (evalresp_logger *log, evalresp_file_format format,
             int use_stdio, const evalresp_response *response)
 {
   int status = EVALRESP_OK, length;
-  char *filename = NULL, *prefix = prefixes[use_stdio && evalresp_fap_output_format ? 4 : format];
+  char *filename = NULL, *prefix = prefixes[use_stdio ? 4 : format];
   length = snprintf (filename, 0, FILENAME_TEMPLATE, prefix,
                      response->network, response->station, response->locid, response->channel);
   if (!(filename = calloc (length + 1, sizeof (*filename))))

libsrc/evalresp/legacy_interface.c
@@ -160,7 +160,6 @@ evresp_itp (char *stalst, char *chalst, char *net_code,
       evalresp_free_options (&options);
       return NULL;
     }
-    options->format_set = 1;
   }
   if (units)
   {
@@ -305,7 +304,6 @@ print_resp_itp (double *freqs, int nfreqs, evalresp_response *first,
       evalresp_free_options (&options);
       return;
     }
-    options->format_set = 1;
   }
 
   for (responses->nresponses = 0, ptr = first; ptr != NULL; responses->nresponses++, ptr = ptr->next)

libsrc/evalresp/output.c
@@ -21,6 +21,7 @@ get_SEEDUNITS (int idx)
   }
   return SEEDUNITS[idx];
 }
+
 int
 evalresp_response_to_char (evalresp_logger *log, const evalresp_response *response,
                            evalresp_file_format format, char **output)

libsrc/evalresp/public_api.h
@@ -194,8 +194,7 @@ typedef struct
   int station_xml; /**< Expect station.xml formatted input (RSEED by default)? */
   evalresp_output_format format; /**< Output format (AMP and PHA by default). */
   evalresp_unit unit; /**< Output unit (displacement by default). */
-  char format_set;
-  int verbose; /**< Vebose output? */
+  int verbose; /**< Verbose output? */
   char unit_set;
 } evalresp_options;
 

src/evalresp.c
@@ -83,7 +83,7 @@ int
 parse_args (int argc, char *argv[], evalresp_options *options, evalresp_filter *filter, evalresp_logger **log)
 {
   int status = EVALRESP_OK, i;
-  int first_switch = 0, flags_argc, option, index;
+  int first_switch = 0, flags_argc, option, index, format_set = 0;
   char *program = argv[0], **flags_argv;
   char *minfreq, *location = NULL, *network = NULL;
 
@@ -184,7 +184,7 @@ parse_args (int argc, char *argv[], evalresp_options *options, evalresp_filter *
 
       case 'r':
         status = evalresp_set_format (*log, options, optarg);
-        options->format_set = 1;
+        format_set = 1;
         break;
 
       case 'S':
@@ -233,7 +233,9 @@ parse_args (int argc, char *argv[], evalresp_options *options, evalresp_filter *
       }
     }
   }
-  if (!options->format_set && options->use_stdio)
+
+  // default appears to be different for stdio (see commit 974d6764)
+  if (!format_set && options->use_stdio)
   {
     options->format = evalresp_fap_output_format;
   }

tests/c/check_input.c
@@ -7,6 +7,7 @@
 #include "evalresp/constants.h"
 #include "evalresp/input.h"
 #include "evalresp/public_api.h"
+#include "evalresp/private.h"
 
 START_TEST (test_slurp_line)
 {

tests/c/check_legacy.c
@@ -9,7 +9,7 @@
 #include "evalresp/stationxml2resp/wrappers.h"
 #include "evalresp_log/log.h"
 #include "old_fctns.h"
-#include <evalresp/private.h>
+#include "evalresp/private.h"
 #include <setjmp.h>
 
 extern int in_epoch (const char *datime, const char *beg_t, const char *end_t);

tests/c/old_fctns.h
@@ -1,4 +1,6 @@
-#include <evalresp/public_api.h>
+#include "evalresp/public_api.h"
+#include "evalresp/private.h"
+
 /**
  * @private
  * @ingroup evalresp_private