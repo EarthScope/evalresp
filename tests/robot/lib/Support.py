370519ea010653fa9c6736fab9ccc201f282e466
Dylan Thies <dylanthies@isti.com>
2017-08-01 13:45:04 -0400

    need to switch direction of slashes for windows

--- tests/robot/lib/Support.py
+++ tests/robot/lib/Support.py
@@ -14,7 +14,7 @@ except Exception:
         csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
         csl.restype = ctypes.c_ubyte
         flags = 1 if isdir(source) else 0
-        if csl(link_name, source, flags) == 0:
+        if csl(link_name, source.replace('/'. '\\'), flags) == 0:
             raise ctypes.WinError()
 
 try:
