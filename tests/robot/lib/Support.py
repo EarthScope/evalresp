51e44ce5de9f226d85fe6a245a24a61d7953f0ce
Dylan Thies <dylanthies@isti.com>
2017-08-01 12:46:58 -0400

    bug fixing robot

--- tests/robot/lib/Support.py
+++ tests/robot/lib/Support.py
@@ -9,10 +9,11 @@ try:
 except Exception:
     def symlink(source, link_name):
         import ctypes
+        from os.path import isdir
         csl = ctypes.windll.kernel32.CreateSymbolicLinkW
         csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
         csl.restype = ctypes.c_ubyte
-        flags = 1 if os.path.isdir(source) else 0
+        flags = 1 if isdir(source) else 0
         if csl(link_name, source, flags) == 0:
             raise ctypes.WinError()
 
