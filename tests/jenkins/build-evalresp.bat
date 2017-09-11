9a7d8a81e3006384cea28bf0474662bfbbfc4bd1
Dylan Thies <dylanthies@isti.com>
2017-09-11 09:28:20 -0400

    windows build script v1

---
+++ tests/jenkins/build-evalresp.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+IF NOT EXIST src (
+    echo "Run this script in the top level directory"
+    EXIT /B 1
+)
+
+RMDIR install
+
+SETLOCAL
+
+set CWD=%CD%
+CALL "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\Common7\\Tools\\VsDevCmd.bat" -arch=x64
+CALL "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\Common7\\Tools\\vsdevcmd\\ext\\vcvars.bat"
+cd %CD%
+nmake /F Makefile.ms
+nmake /F install-for-robot
+
+ENDLOCAL
